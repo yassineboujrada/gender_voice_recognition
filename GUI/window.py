a6165a4505917bb30cf653305252ff8163134d77
yassineboujrada <yassine.boujrada@gmail.com>
2022-05-03 14:28:45 +0000

    final GUI design with test if it predict cool or not

--- GUI/window.py
+++ GUI/window.py
@@ -16,13 +16,12 @@ class window_tk(tk.Frame):
         
         ###################################  designe my GUI  #################################
 
-        self.root.geometry("650x400")
+        self.root.geometry("600x350")
         self.root.title("vocal prediction")
         self.root.resizable(0,0)
         self.root.configure(background="#17122b")
 
         self.warning=tk.Label(self.root, text="This voice save every 5 seconds",bg="#17122b",font=("Lucida Sans", 12))
-        # self.checktime=tk.Entry(self.root, textvariable = "number_of_duration", width = 8)
         self.photo_image = tk.PhotoImage(file="../gender_voice_recognition/GUI/start.png")
         self.voice=tk.Button(self.root,image=self.photo_image ,command=self.save_voice, height = 120, width = 100)
         
@@ -32,22 +31,16 @@ class window_tk(tk.Frame):
         self.warning.configure(foreground="red")
         self.gender.configure(foreground="green",bg="#17122b",font=("Century Gothic", 11))
         self.gender_predict.configure(foreground="green",bg="#17122b",font=("Century Gothic", 11))
-        self.warning.place(x=215,y=20)
-        self.voice.place(x=270,y=180)
-        # self.checktime.place(x=120,y=120)
-        self.gender.place(x=220,y=340)
-        self.gender_predict.place(x=350,y=340)
+        self.warning.place(x=190,y=20)
+        self.voice.place(x=265,y=120)
+        self.gender.place(x=195,y=290)
+        self.gender_predict.place(x=325,y=290)
         
         
     ####################################3  function to save voice to '.wav'    
     def save_voice(self):
         freq = 44100
-        
-        # if self.checktime.get() == "":
-        duration=2
-        # else:
-        #     duration = int(self.checktime.get())
-            
+        duration=3
         recording = sd.rec(int(duration * freq),samplerate=freq, channels=2)
         sd.wait()
         wv.write("../gender_voice_recognition/voice/saving_voice.wav", recording, freq, sampwidth=2)
